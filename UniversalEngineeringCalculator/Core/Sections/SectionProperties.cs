8e32a4a9080228e407a84d28dfec0ad07f8ac1f8
Serebrovsky Sergey Maksimovich. <61110689+Mika-dot@users.noreply.github.com>
2026-08-24 18:34:23 +0300

    Add engineering section property model

---
+++ UniversalEngineeringCalculator/Core/Sections/SectionProperties.cs
@@ -0,0 +1,9 @@
+namespace UniversalEngineeringCalculator.Core.Sections;
+
+public class SectionProperties
+{
+    public string Name { get; set; } = "Custom";
+    public double Area { get; set; }
+    public double MomentOfInertiaX { get; set; }
+    public double SectionModulusX { get; set; }
+}
