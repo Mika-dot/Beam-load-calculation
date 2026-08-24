d196fc0f01ab48a6623fb9b8778c20b2f4dee160
Serebrovsky Sergey Maksimovich. <61110689+Mika-dot@users.noreply.github.com>
2026-08-24 18:34:20 +0300

    Add beam calculation result model

---
+++ UniversalEngineeringCalculator/Core/Beam/BeamResult.cs
@@ -0,0 +1,10 @@
+namespace UniversalEngineeringCalculator.Core.Beam;
+
+public class BeamResult
+{
+    public double LeftReaction { get; set; }
+    public double RightReaction { get; set; }
+    public double MaximumMoment { get; set; }
+    public double MaximumShear { get; set; }
+    public double CriticalPosition { get; set; }
+}
